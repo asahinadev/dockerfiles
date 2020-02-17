0ad4ebd0f6e00820dff3dc4390dccbbd37c6e5c0
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-02-18 06:30:58 +0900

    asahinadev/php:7.2-centos6

---
+++ php/7.2-centos6/build-run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+app=php:7.2-centos6
+tag=asahinadev/php:7.2-centos6
+
+docker stop       $app
+docker rm         $app
+docker build -t   $app .
+docker run --name $app -d -p 10080:80 $app
+
+docker login
+docker tag    $app $tag
+docker push        $tag
