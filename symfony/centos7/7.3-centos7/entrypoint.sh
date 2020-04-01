63a6dc0d3c5e0e9d9c16c39323512919849da565
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-04-01 21:03:28 +0900

    Symfony Framework 用

---
+++ symfony/centos7/7.3-centos7/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SERVER_NAME=${SERVER_NAME:=localhost}
+ALLOWOVERRIDE=${ALLOWOVERRIDE:=ALL}
+DocumentRoot=${DocumentRoot:=/var/www/html/public}
+
+cd /etc/httpd/conf
+sed -i.org -e "s/^#ServerName .*$/ServerName $SERVER_NAME/g" httpd.conf
+sed -i     -e "s/AllowOverride None/AllowOverride $ALLOWOVERRIDE/g" httpd.conf
+sed -i     -e "s|^DocumentRoot .*$|DocumentRoot $DocumentRoot|g" httpd.conf
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
+cd /var/www/html
+
+if test -f composer.lock;then
+    yes | composer update
+elif test -f composer.json;then
+    yes | composer install
+else
+    yes | composer create-project --prefer-dist symfony/skeleton .
+fi
+
+/usr/sbin/httpd -DFOREGROUND
