19a6984fc3756d2cbc9f45d840aba54ba3d3a084
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-04-01 21:03:02 +0900

    ドキュメントルート調整

--- cakephp/centos8/7.4-centos8/startup.sh
+++ cakephp/centos8/7.4-centos8/startup.sh
@@ -2,12 +2,12 @@
 
 SERVER_NAME=${SERVER_NAME:=localhost}
 ALLOWOVERRIDE=${ALLOWOVERRIDE:=ALL}
+DocumentRoot=${DocumentRoot:=/var/www/html/webroot}
 
 cd /etc/httpd/conf
 sed -i.org -e "s/^#ServerName .*$/ServerName $SERVER_NAME/g" httpd.conf
 sed -i     -e "s/AllowOverride None/AllowOverride $ALLOWOVERRIDE/g" httpd.conf
-
-diff -u httpd.conf.org httpd.conf
+sed -i     -e "s|^DocumentRoot .*$|DocumentRoot $DocumentRoot|g" httpd.conf
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
@@ -15,10 +15,12 @@ php -r "unlink('composer-setup.php');"
 
 cd /var/www/html
 
-if test -f composer.json;then
+if test -f composer.lock;then
+    yes | composer update
+elif  test -f composer.json;then
     yes | composer install
 else
-    yes | composer create-project --prefer-dist cakephp/app:^3.8 .
+    yes | composer create-project --prefer-dist cakephp/app .
 fi
 
 if test -f bin/cake;then
@@ -37,6 +39,5 @@ fi
 
 mkdir /run/php-fpm
 php-fpm -c /var/www
-/usr/sbin/httpd -M
 /usr/sbin/httpd -DFOREGROUND
 
